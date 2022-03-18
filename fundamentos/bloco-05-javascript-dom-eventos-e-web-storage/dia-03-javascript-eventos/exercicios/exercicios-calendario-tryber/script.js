d7f3f5e7c8b7c7a46d9eb475772bfa6ca45235bc
Leandro Camargo <leandro.bcamargo@gmail.com>
2022-03-18 20:58:52 -0300

    exercicio eventos parte 8

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/exercicios-calendario-tryber/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/exercicios-calendario-tryber/script.js
@@ -143,3 +143,17 @@ function addTask(string) {
   spanParent.appendChild(spanTask);
 }
 addTask("cozinhar");
+
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+function addColor(string) {
+  let divTask = document.createElement("div");
+  divTask.className = "task";
+  divTask.style.backgroundColor = string;
+  let divTaskParent = document.querySelector(".my-tasks");
+  divTaskParent.appendChild(divTask);
+}
+addColor("teal");
+
