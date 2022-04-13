56c8319661f0c748a2a22973b2a474de30c5f22e
Leandro Camargo <leandro.bcamargo@gmail.com>
2022-04-13 14:04:37 -0300

    notas de aula e exercicios 2

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicios/exercicios-agora-a-pratica-5.js
@@ -0,0 +1,38 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+function containsA() {
+  // escreva seu código aqui
+  const aCounter = names.reduce((totalNumberAs, name) => {
+    const subtotal = totalNumberAs + name.split('').reduce((numberAsPerWord, character) => {
+      if (character === 'A' || character === 'a') {
+        return numberAsPerWord + 1;
+      }
+      return totalNumberAs;
+    })
+  }, 0);
+  return aCounter
+}
+
+console.log(containsA());
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, curr) =>
+     acc + curr.split('').reduce((acumulator, current) => {
+        if (current === 'a' || current === 'A') return acumulator + 1;
+        return acumulator;
+     }, 0), 0);
+}
+console.log(containsA());
