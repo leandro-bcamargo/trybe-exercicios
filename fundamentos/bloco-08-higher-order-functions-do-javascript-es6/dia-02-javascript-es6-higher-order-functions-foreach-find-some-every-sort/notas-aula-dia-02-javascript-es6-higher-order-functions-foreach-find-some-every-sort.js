ec0aec222632190c897139a02914397dddf20406
Leandro Camargo <leandro.bcamargo@gmail.com>
2022-04-19 00:04:37 -0300

    refazendo exercicios para revisao

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/notas-aula-dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/notas-aula-dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort.js
@@ -324,4 +324,12 @@ console.log(points); // [1, 5, 10, 25, 40, 100]
 
 // Veja agora se realizarmos uma simples modificação no retorno da função que ordena os números e veja o que acontece:
 
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => b - a);
+console.log(points); // [ 100, 40, 25, 10, 5, 1 ]
+
+// Para visualizar como o sort funciona, brinque com este exemplo feito no CodePen.
+
+
+
 
